7a2a41678ca7ee82f1ec7c89d149b6848a3e6bd4
Patata <patattta93@gmail.com>
2023-09-08 03:50:17 +0900

    feat: api 모듈 수정 및 로컬캐싱 구현

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -1,3 +1,4 @@
+import { KeywordItem } from '@/types'
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
 const getClient = (baseURL: string) => {
@@ -6,9 +7,34 @@ const getClient = (baseURL: string) => {
 
 class ApiClient {
   client: AxiosInstance;
+  queryString: { query?: string } = {};
 
   constructor(baseURL: string) {
     this.client = getClient(baseURL);
+
+    this.client.interceptors.request.use((config) => {
+      if (config.params && config.params.q) {
+        this.queryString['query'] = config.params.q;
+      }
+      return config;
+    });
+
+    this.client.interceptors.response.use((response) => {
+      const query = this.queryString.query;
+      if (query) {
+        response.data.sort((a: KeywordItem, b: KeywordItem) => {
+          const startsWithA = a.sickNm.startsWith(query);
+          const startsWithB = b.sickNm.startsWith(query);
+          if (!startsWithA && startsWithB) {
+            return 1;
+          } else if (startsWithA && !startsWithB) {
+            return -1;
+          }
+          return 0;
+        });
+      }
+      return response;
+    });
   }
 
   get(endPoint: string, config?: AxiosRequestConfig) {
